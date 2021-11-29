470a0004adae433a3b8e35c886cce1871a109c8e
frameur <francoisramos6121@gmail.com>
2021-11-29 22:17:50 +0100

    init vitrine folio

---
+++ react-react-vitrine/src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Montypython",
+    date: "Janvier 2021",
+    languages: ["Javscript", "librairie javascript", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/montypython.PNG",
+    link: "https://projetmonty.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Brasseur de France",
+    date: "Mars 2021",
+    languages: ["Reactjs", "Msql"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/brewerbeer.PNG",
+    link: "http://www.github.com",
+  },
+  {
+    id: 3,
+    title: "API Carnet de cuisine",
+    date: "Avril 2021",
+    languages: ["Vuejs", "Mongodb", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/cookinstock.PNG",
+    link: "http://www.github.com",
+  },
+  {
+    id: 4,
+    title: "Portfolio Gatsby",
+    date: "Juillet 2020",
+    languages: ["Reactjs", "Gatsby", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/gatsbyportfolio2.PNG",
+    link: "https://portfolioframgatsby.netlify.app/",
+  },
+];
